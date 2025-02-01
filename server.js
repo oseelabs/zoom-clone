823de3e56aedb3159d129077eb714b45ec8d1776
Lazaro Osee <odiraoseelazaro@students.tukenya.ac.ke>
2025-02-01 17:46:55 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const { v4: uuidV4 } = require('uuid');
+
+const app = express();
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.redirect(`/${uuidV4()}`);
+});
+
+app.get('/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room });
+});
+
+io.on('connection', socket => {
+    socket.on('join-room', (roomId, userId) => {
+        socket.join(roomId);
+        socket.to(roomId).emit('user-connected', userId);
+    });
+})
+
+server.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
